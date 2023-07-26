d995df20e478c3d3f5648392a1b86116a686c81d
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-26 21:17:07 +0530

    Created Abstract Syntax Tree

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "Zeon/Programming-Language/token"
+
+type Node interface{
+	TokenLiteral() string
+}
+
+type Statement interface{
+	Node
+	statementNode()
+}
+
+type Expression interface{
+	Node
+	expressionNode()
+}
+
+type Program struct{
+	Statements []Statement
+}
+
+type Identifier struct{
+	Token token.Token
+	Value string
+}
+
+func (p *Program) TokenLiteral() string{
+	if len(p.Statements) > 0{
+		return p.Statements[0].TokenLiteral()
+	}else{
+		return ""
+	}
+}
+
+type LetStatement struct{
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {return i.Token.Literal}
