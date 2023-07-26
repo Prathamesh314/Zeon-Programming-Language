e8ff0f2c7eccc372fe619be846b67f1bb9153107
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-26 16:12:41 +0530

    Added properties to read numbers and keywords

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -29,8 +29,40 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+func isLetter(ch byte) bool{
+	return 'a' <= ch && ch <= 'z' || 'A'<= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool{
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) readNumber() string{
+	position := l.position
+	for isDigit(l.ch){
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readIdentifier() string{
+	position := l.position
+	for isLetter(l.ch){
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace(){
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r'{
+		l.readChar()
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -52,6 +84,17 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch){
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}else if isDigit(l.ch){
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 	l.readChar()
 	return tok
--- token/token.go
+++ token/token.go
@@ -7,6 +7,18 @@ type Token struct{
 	Literal string
 }
 
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType{
+	if tok, ok := keywords[ident]; ok{
+		return tok
+	}
+	return IDENT
+}
+
 const(
 	ILLEGAL = "ILLEGAL"
 	EOF = "EOF"
