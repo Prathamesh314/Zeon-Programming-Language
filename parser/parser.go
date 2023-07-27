52b14ef4e3ca2c44f0a7fa2c7195cd6efa04d57c
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-27 17:07:56 +0530

    We can now parse prefix expressions!

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"Zeon/Programming-Language/lexer"
 	"Zeon/Programming-Language/token"
 	"fmt"
+	"strconv"
 )
 
 const (
@@ -38,6 +39,9 @@ func New(l *lexer.Lexer) *Parser {
 	}
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.MINUS,p.parsePrefixExpression)
+	p.registerPrefix(token.BANG,p.parsePrefixExpression)
 	p.nextToken()
 	p.nextToken()
 
@@ -107,6 +111,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
@@ -164,3 +169,34 @@ type (
 	prefixParseFn func() ast.Expression
 	infixParseFn  func(ast.Expression) ast.Expression
 )
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType){
+	msg := fmt.Sprintf("no prefix parse function for %s found",t)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression{
+	expression := &ast.PrefixExpression{
+		Token: p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
+}
