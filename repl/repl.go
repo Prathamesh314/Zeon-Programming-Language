af9abd18e9940c957a57775ec5a58d353cf88f5a
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-30 12:36:52 +0530

    Evaluation of boolean,prefix and infix expressions completed

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"Zeon/Programming-Language/evaluator"
 	"Zeon/Programming-Language/lexer"
 	"Zeon/Programming-Language/parser"
 	"bufio"
@@ -31,8 +32,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
