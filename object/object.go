0f7f7eadd81a7035d23ee75027b6168f420ef0e4
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-30 11:56:22 +0530

    Added integer, boolean, null objects

--- object/object.go
+++ object/object.go
@@ -1,9 +1,35 @@
 package object
 
+import "fmt"
+
 type ObjectType string
 
-type Object interface{
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
 
+type Integer struct {
+	Value int64
+}
+
+type Boolean struct {
+	Value bool
+}
+
+type Null struct{}
+
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+func (n *Null) Inspect() string  { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
