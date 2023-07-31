884226bec48dafc535fe729530672f7ac9d17ac0
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-07-31 10:30:21 +0530

    Set up environment for variable binding

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -53,11 +53,22 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(val) {
 			return val
 		}
+		env.Set(node.Name.Value, val)
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
 
 	}
 
 	return nil
 
+}
+
+func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
+	val, ok := env.Get(node.Value)
+	if !ok {
+		return newError("identifier not found: " + node.Value)
+	}
+	return val
 }
 
 func isError(obj object.Object) bool {
