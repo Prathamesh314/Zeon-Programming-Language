f94116d24fb58aeda2b38796a322203e1185fb14
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2024-03-09 00:13:24 +0530

    updated lexer.go

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -106,6 +106,7 @@ func (l *Lexer) NextToken() token.Token {
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
 	case '+':
+		
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
